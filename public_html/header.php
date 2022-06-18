f90afe29347aaa1669f711f012abbb7bb012e4d5
McKayRussell <45216953+McKayRussell@users.noreply.github.com>
2022-06-18 11:59:38 -0700

    testing

---
+++ public_html/header.php
@@ -0,0 +1,34 @@
+<!-- 
+  PROJECT: Utilizing Data Mining Technologies to Analyze the Physical and
+           Cognitive Benefits of Golf Therapy on Seniors
+  AUTHORS: Steven Merino, McKay Russell,
+           Clemente Rodriguez, Zachary Scholefield
+  ADVISOR: Dr. Chengwei Lei
+  SEMESTERS: Fall 2021, Spring 2022
+ -->
+
+ <!-- Contains stylesheets for fonts and basic header metadata -->
+<?php define("PAGE_TITLE", "Señor Projectors Golf Project"); ?>
+<html> 
+    <head>
+        <title><?php echo PAGE_TITLE?></title>
+        <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+        rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+        crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet">
+        <!-- <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@100&display=swap" rel="stylesheet"> -->
+        <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@300&family=Cantarell:ital,wght@1,700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet">
+        <meta property="og:title" content="Señor Projectors Golf Project" />
+        <meta property="og:description" content="Senior Project by Zachary Scholefield, Clemente Rodriguez, McKay Russell, and Steven Merino" />
+        <meta property="og:image" content="https://cs.csub.edu/~zscholefield/seniorProjectors/Web_Site/logo4.jpg" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="./style.css" />
+    </head> 
+</html>
